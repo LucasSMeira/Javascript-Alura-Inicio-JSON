f98ae5d31309bedc5904ddb62ab3868f52071174
Lucas Meira <lucasmeiraprofessional@gmail.com>
2024-03-26 12:35:46 -0300

    Passo 1

---
+++ chamadoutilizarRequire.js
@@ -0,0 +1,8 @@
+
+const dadosIMP =  require('./JSON/dados.json');
+
+console.log(dadosIMP);
+const chaves = Object.keys(dadosIMP);
+const valores = Object.values(dadosIMP);
+console.log(chaves);
+console.log(valores);
